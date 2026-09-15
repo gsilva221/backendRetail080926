28a89f70b32b1111eb834868b42f457bdd903c83
unknown <sistemas@VRE_LAB20834.inacap.cl>
2026-09-15 16:55:28 -0300

    agregando admin

--- serviciosApp/admin.py
+++ serviciosApp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Servicio, PrecioServcio
 # Register your models here.
+
+@admin.register(Servicio)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ('id','nombre', 'descripcion', 'disponibilidad')
+    search_fields = ('nombre')
+    list_filter = ('disponibilidad',)
+    ordering = ('id',)
+
+
+@admin.register(PrecioServcio)
+class PrecioServcioAdmin(admin.ModelAdmin):
+    list_display = ('id','servicio', 'precio', 'decuento', 'moneda', 'observacion')
+    search_fields = ('servicio__nombre',)
+    list_filter = ('moneda',)
+    ordering = ('id',)    
--- serviciosApp/models.py
+++ serviciosApp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
 # Create your models here.
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    disponibilidad = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class PrecioServcio(models.Model):
+    servicio = models.OneToOneField(
+        Servicio, 
+        on_delete=models.CASCADE
+        )
+    precio = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2
+        )
+    decuento = models.DecimalField(
+        max_digits=5, 
+        decimal_places=2, 
+        default=0.00
+        )
+    moneda = models.CharField(
+        max_length=10, 
+        default='CLP'
+        )
+    observacion = models.TextField(
+        blank=True, 
+        max_length=200
+        )
+
+    def __str__(self):
+        return f"{self.servicio.nombre} - {self.precio} {self.moneda}"
